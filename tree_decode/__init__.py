2ef5a719d49d39a6aa758a4a34c2f16f4bc979cd
gfyoung <gfyoung17@gmail.com>
2017-12-01 22:38:59 -0500

    Surface test method in tree-decode init file

--- tree_decode/__init__.py
+++ tree_decode/__init__.py
@@ -8,3 +8,21 @@ surrounding decision trees from scikit-learn, making it
 easier to understand how they work and more importantly,
 to diagnose their issues when they produce unexpected results.
 """
+
+
+def test():
+    """
+    Run unit tests on the current tree_decode installation.
+    """
+
+    try:
+        import pytest  # noqa
+    except ImportError:
+        raise ImportError("pytest not found. Please install "
+                          "with `pip install pytest`")
+
+    from subprocess import call
+    from os.path import dirname
+
+    directory = dirname(__file__)
+    call(["pytest", directory])
