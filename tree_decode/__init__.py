279360e5f757b6929e09e5d36d79c48555d58fea
gfyoung <gfyoung17@gmail.com>
2017-12-01 21:48:22 -0500

    Add module docstring for tree-decode

--- tree_decode/__init__.py
+++ tree_decode/__init__.py
@@ -1,3 +1,10 @@
 from tree_decode.api import *  # noqa
 
 __version__ = "0.1.0"
+
+__doc__ = """
+tree_decode - Library that helps to remove the black-box
+surrounding decision trees from scikit-learn, making it
+easier to understand how they work and more importantly,
+to diagnose their issues when they produce unexpected results.
+"""
